e0aedaa1b2890045458ce884392a354a5b29667a
Abdul Qadeer <abdulqadeer1999>
2020-12-16 14:35:36 +0500

    switchcase assignment

---
+++ app.js
@@ -0,0 +1,75 @@
+// let n = +prompt("Enter a number")
+
+// function isEven (n){
+//     if(n%2 === 0){
+//         return true;
+//     }else {
+//         return false;
+//     }
+// }
+
+// console.log(isEven())
+
+
+
+// let x = +prompt("Enter a number")
+// function test_prime(n)
+// {
+
+//   if (n===1)
+//   {
+//     return false;
+//   }
+//   else if(n === 2)
+//   {
+//     return true;
+//   }else
+//   {
+//     for(var x = 2; x < n; x++)
+//     {
+//       if(n % x === 0)
+//       {
+//         return false;
+//       }
+//     }
+//     return true;  
+//   }
+// }
+
+// console.log(test_prime())
+
+
+
+// let studentGrade = "";
+// studentGrade = prompt("Enter grade")
+
+// switch (studentGrade) {
+//     case "A+":
+//     case "A":
+//         console.log("Well done")
+//         break;
+//     case "B":
+//         console.log("Good work")
+//         break;
+//     case "C":
+//         console.log("Need to work");
+//         break;
+//     case "D":
+
+//         console.log("Hard work required")
+//         break;
+//     default:
+//         console.log("Nothing to match")
+// }
+
+
+// let choice = "y";
+// do {
+//     let num1 = +prompt("Enter first num1")
+//     let num2 = +prompt("Enter num2")
+//     let add = num1 + num2;
+//     alert(add)
+
+// }while(choice === prompt("Do you want Again y/n"));
+
+
